d802b64f3ff65cd226782dcebd38d318ea7c2cd8
Nathan Lin <nathanlukelin@gmail.com>
2020-09-14 14:41:17 +0000

    Tiny changes to match what autograder expects

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
-    public static String countABC(String str) {
+    public static int countABC(String str) {
         //A CODE
         int length_char = str.length(); //length_char is the length of the inputted string
         int i_countA = 0; //initializes the counter for A freq count
@@ -43,25 +43,28 @@ public class MyMain {
             String A_string = Integer.toString(i_countA);
             String B_string = Integer.toString(i_countB);
             String C_string = Integer.toString(i_countC);
-            return ("A: " + A_string + " B: " + B_string + " C: " + C_string);
+            // return ("A: " + A_string + " B: " + B_string + " C: " + C_string);
+            return i_countA + i_countB + i_countC;
         //NOTE, I'm trying to figure out how to take SPACES into account - the code currently only works whenever no spaces are involved in the string.
         }
     // }
 
     // Checks to see whether or not str contains 'The' or 'the'
-    public static String containsThe(String str) {
+    public static boolean containsThe(String str) {
       String str_CAP = str.toUpperCase();
       int check_the = str_CAP.indexOf("THE");
         if (check_the>=0){ //if positive;
-          return("Contains the string the");
+        //   return("Contains the string the");
+            return true;
         }
         else{
-          return("Does not contain the string the");
+        //   return("Does not contain the string the");
+            return false;
         }
     }
 
     // Checks whether str is a palindrome or not
-    public static int isPalindrome(String str) {
+    public static boolean isPalindrome(String str) {
       System.out.println("-------");
       System.out.println("1 = Palindrome; 2 = Not a palindrome");
       System.out.println("-------");
@@ -71,10 +74,12 @@ public class MyMain {
          rev = rev + str.charAt(i);
       }
       if (str.equals(rev)){
-         return(1);
+        //  return(1);
+        return true;
       }
       else{
-         return(2);
+        //  return(2);
+        return false;
       }
       
     }
